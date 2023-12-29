77385eb19cced2fee6e8662c850a5237f0b4d491
Alok232101 <alokg5251@gmail.com>
2023-12-29 16:06:15 +0530

    add files

---
+++ Admin/includes/db.php
@@ -0,0 +1,4 @@
+<?php 
+        $con = mysqli_connect("localhost","root","","impulse101");
+        mysqli_close($con);
+        ?>      
---
+++ Includes/db.php
@@ -0,0 +1,9 @@
+<?php 
+
+        $con = mysqli_connect("localhost","root","","impulse101");
+      
+        if (mysqli_connect_errno()) {
+                echo "Failed to connect to MySql " . mysqli_connect_error();
+        }
+        mysqli_close($con);
+        ?>
